483473b0a5b04851cc15fed4bc8d39a2ab124ddc
Daniel Guinter <guinterdaniel@gmail.com>
2024-01-26 20:18:08 -0300

    Created project backend

---
+++ backend/src/test/java/com/dguinter/Marcenaria/MarcenariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dguinter.Marcenaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcenariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
