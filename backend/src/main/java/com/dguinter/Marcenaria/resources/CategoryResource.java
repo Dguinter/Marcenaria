6fc7802a8be48bab0a85c084697c907fb0d73447
Daniel Guinter <guinterdaniel@gmail.com>
2024-01-29 23:35:34 -0300

    Create H2 Database

--- backend/src/main/java/com/dguinter/Marcenaria/resources/CategoryResource.java
+++ backend/src/main/java/com/dguinter/Marcenaria/resources/CategoryResource.java
@@ -1,27 +1,25 @@
 package com.dguinter.Marcenaria.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dguinter.Marcenaria.entities.Category;
+import com.dguinter.Marcenaria.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	@Autowired
+	private CategoryService service;
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Cadeira"));
-		list.add(new Category(2L, "Mesa"));
-		list.add(new Category(3L, "Floreira"));
-		list.add(new Category(4L, "Caixa de areia"));
-		list.add(new Category(5L, "Mesa infantil"));
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
